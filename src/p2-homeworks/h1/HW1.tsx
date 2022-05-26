c3327af0f8be7f659552dfa43bd8fc80a93b7712
e-doschechnikova <evadrobakova@gmail.com>
2022-05-26 17:39:55 +0300

    fix message

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,33 +1,31 @@
-import React from 'react'
+import React from "react";
+import Message from "./Message";
 
 const messageData = {
-    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
-    time: '22:00',
-}
+  avatar:
+    " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_eTHoHEyBCjd-p5MA-pUIvItDoaKI7e-A5KsHZrC715mj_B3YtZPF2Cw26mpv2Xzijqk&usqp=CAU",
+  name: "Lama",
+  message: "hello",
+  time: "22:00",
+};
 
 function HW1() {
-    return (
-        <div>
-            <hr/>
-            homeworks 1
-
-            {/*should work (должно работать)*/}
-
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeMessage/>*/}
-            <hr/>
-        </div>
-    )
+  return (
+    <div>
+      <hr />
+      homeworks 1{/*should work (должно работать)*/}
+      <Message
+        avatar={messageData.avatar}
+        name={messageData.name}
+        message={messageData.message}
+        time={messageData.time}
+      />
+      <hr />
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeMessage/>*/}
+      <hr />
+    </div>
+  );
 }
 
-export default HW1
+export default HW1;
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./Message.module.css";
 
 type messageType = {
   avatar: string;
@@ -8,7 +9,18 @@ type messageType = {
 };
 
 function Message(props: messageType) {
-  return <div></div>;
+  return (
+    <div className={styles.content}>
+      <div>
+        <img src={props.avatar} alt="avatar" />
+      </div>
+      <div className={styles.messageContent}>
+        <div className={styles.name}>{props.name}</div>
+        <div className={styles.message}>{props.message}</div>
+        <div className={styles.time}>{props.time}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Message;
