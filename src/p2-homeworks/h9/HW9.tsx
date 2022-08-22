846c90271241be22f57e662762ab1452d544a705
e-doschechnikova <evadrobakova@gmail.com>
2022-08-22 21:10:53 +0300

    add HW9

--- src/p2-homeworks/h9/HW9.tsx
+++ src/p2-homeworks/h9/HW9.tsx
@@ -4,13 +4,12 @@ import Clock from './Clock'
 function HW9() {
     return (
         <div>
-            <hr/>
             homeworks 9
 
             {/*should work (должно работать)*/}
             <Clock/>
 
-            <hr/>
+
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeClock/>*/}
             <hr/>
