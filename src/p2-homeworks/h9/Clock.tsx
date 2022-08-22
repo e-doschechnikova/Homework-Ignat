9d225576acbde113298cab97e0d407550223c0d0
e-doschechnikova <evadrobakova@gmail.com>
2022-08-22 22:20:00 +0300

    finished HW9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,50 +1,56 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import styles from "./Clock.module.css"
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
+
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const hours = (date.getHours() < 10) ? "0" + date.getHours() : date.getHours()
+    const minutes = (date.getMinutes() < 10) ? "0" + date.getMinutes() : date.getMinutes()
+    const seconds = (date.getSeconds() < 10) ? "0" + date.getSeconds() : date.getSeconds()
 
+    const stringTime = (hours + ":" + minutes + ":" + seconds)
+    const stringDate = (new Date().toLocaleDateString())
     return (
-        <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
+        <div className={styles.dataTimeContainer}>
+            <div className={styles.time}
+                 onMouseEnter={onMouseEnter}
+                 onMouseLeave={onMouseLeave}
             >
                 {stringTime}
             </div>
 
             {show && (
-                <div>
+                <div className={styles.date}>
                     {stringDate}
                 </div>
             )}
-
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
-
+            <div className={styles.buttonsContainer}>
+                <SuperButton red onClick={start}>start</SuperButton>
+                <SuperButton red onClick={stop}>stop</SuperButton>
+            </div>
         </div>
     )
 }
