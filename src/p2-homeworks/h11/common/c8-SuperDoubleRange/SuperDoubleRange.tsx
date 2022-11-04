c872db3f2576fd5ad90e4e1fc8e198ada302b44c
e-doschechnikova <evadrobakova@gmail.com>
2022-11-04 13:04:19 +0300

    add HW11

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,23 +1,60 @@
 import React from 'react'
+import {Slider, withStyles} from "@material-ui/core";
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
+    onChangeRange?: (event: React.ChangeEvent<{}>, value: number | number[]) => void
+    value?: [number, number],
+    min?: number,
+    max?: number,
+    step?: number,
+    disable?: boolean
+
     // min, max, step, disable, ...
 }
 
+
+const MySlider = withStyles({
+    root: {
+        color: '#dabdab'
+    },
+    thumb: {
+        height: 20,
+        width: 20,
+        backgroundColor: '#4c2f27',
+        borderRadius: 50,
+        marginTop: -6,
+        '&:hover': {
+            boxShadow: 'rgba(177, 152, 123, 0.31) 0 0 0 8px'
+        },
+    },
+    track: {
+        height: 8
+    },
+    rail: {
+        color: '#dabdab',
+        height: 8,
+        borderRadius: 50,
+        opacity: 1
+    }
+})(Slider);
+
+
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
-        onChangeRange, value,
+        onChangeRange, value, min, max, step, disable
         // min, max, step, disable, ...
     }
 ) => {
     // сделать самому, можно подключать библиотеки
-
     return (
-        <>
-            DoubleRange
-        </>
+        <div style={{width: "150px"}}>
+            <MySlider onChange={onChangeRange}
+                   value={value}
+                   min={min}
+                   max={max}
+                   step={step}
+                   disabled={disable}/>
+        </div>
     )
 }
 
